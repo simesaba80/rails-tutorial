0f2fc7e7160c914e29d6cc46bf5b46b474b79b48
simesaba80 <simesaba1120@gmail.com>
2026-03-29 00:03:14 +0900

    メール送信まで終わり

---
+++ app/controllers/subscribers_controller.rb
@@ -0,0 +1,9 @@
+class SubscribersController < ApplicationController
+  allow_unauthenticated_access
+  before_action :set_product
+
+  def create
+    @product.subscribers.where(subscriber_params).first_or_create
+    redirect_to @product, notice: "You are now subscribed."
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,10 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
   root "products#index"
-  resources :products
+  resources :products do
+    resources :subscribers, only: [ :create ]
+  end
+  resource :unsubscribe, only: [ :show ]
 
   # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
   # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
