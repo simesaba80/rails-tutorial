3dd19c577899bb6bcdc9c5c6a59feec2c17a22d2
simesaba80 <simesaba1120@gmail.com>
2026-03-29 00:06:40 +0900

    メール送信まで終わり

--- app/controllers/subscribers_controller.rb
+++ app/controllers/subscribers_controller.rb
@@ -6,4 +6,13 @@ class SubscribersController < ApplicationController
     @product.subscribers.where(subscriber_params).first_or_create
     redirect_to @product, notice: "You are now subscribed."
   end
+
+  private
+    def set_product
+      @product = Product.find(params[:product_id])
+    end
+
+    def subscriber_params
+      params.expect(subscriber: [ :email ])
+    end
 end
