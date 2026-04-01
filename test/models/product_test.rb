849e93893d26599b0514fae40c4d40826a3153ec
simesaba80 <simesaba1120@gmail.com>
2026-04-01 15:11:39 +0900

    テストまで終わり

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -4,4 +4,15 @@ class ProductTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  include ActionMailer::TestHelper
+
+  test "sends email notification when back in stock" do
+    product = products(:tshirt)
+
+    product.update(inventory_count: 0)
+
+    assert_emails 2 do
+      product.update(inventory_count: 99)
+    end
+  end
 end
